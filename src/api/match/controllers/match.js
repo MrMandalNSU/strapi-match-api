6cd7bd45ae15b0af0eccbee5e7121be6ba4a3385
Sudipta Mandal <sudipta.mandal@northsouth.edu>
2025-10-04 18:18:18 +0600

    added pagination

--- src/api/match/controllers/match.js
+++ src/api/match/controllers/match.js
@@ -1,15 +1,13 @@
 "use strict";
 
-/**
- * match controller
- */
-
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::match.match", ({ strapi }) => ({
   async upcoming(ctx) {
     try {
       const now = new Date();
+
+      const { limit = 10, offset = 0 } = ctx.query;
 
       const upcomingMatches = await strapi.db
         .query("api::match.match")
@@ -18,14 +16,40 @@ module.exports = createCoreController("api::match.match", ({ strapi }) => ({
             date: { $gte: now.toISOString() },
           },
           orderBy: { date: "asc" },
+          limit: parseInt(limit),
+          offset: parseInt(offset),
         });
 
+      const total = await strapi.db.query("api::match.match").count({
+        where: {
+          date: { $gte: now.toISOString() },
+        },
+      });
+
       if (!upcomingMatches || upcomingMatches.length === 0) {
-        ctx.status = 200;
-        return { message: "No upcoming matches available" };
+        return {
+          data: [],
+          meta: {
+            pagination: {
+              limit: parseInt(limit),
+              offset: parseInt(offset),
+              total: 0,
+            },
+          },
+          message: "No upcoming matches available",
+        };
       }
 
-      return upcomingMatches;
+      return {
+        data: upcomingMatches,
+        meta: {
+          pagination: {
+            limit: parseInt(limit),
+            offset: parseInt(offset),
+            total: total,
+          },
+        },
+      };
     } catch (error) {
       ctx.status = 500;
       return { error: "An error occurred while fetching upcoming matches" };
